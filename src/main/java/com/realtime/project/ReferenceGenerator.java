8c7ae0e1d8bbd01f35fdd6901e2e802d487f52f3
Jerry-Lindstrom <Jerrylindstrom@hotmail.se>
2013-11-11 13:31:34 +0100

    no longer a thread

--- src/main/java/com/realtime/project/ReferenceGenerator.java
+++ src/main/java/com/realtime/project/ReferenceGenerator.java
@@ -5,38 +5,26 @@ import java.awt.*;
 import java.awt.event.*;
 import se.lth.control.*;
 
-public class ReferenceGenerator extends Thread {
+public class ReferenceGenerator{
 
-    private double amplitude;
     private int period;
     private double ref;
 
-    private class RefGUI {
-	private JPanel paramsLabelPanel = new JPanel();
-	private JPanel paramsFieldPanel = new JPanel();
-        private BoxPanel paramsPanel = new BoxPanel(BoxPanel.HORIZONTAL);
-        private JTextField paramsAmpField = new JTextField();
-        private JTextField paramsPeriodField = new JTextField();
+   
   
-    public ReferenceGenerator(double pos) {
-	amplitude = pos;
-    }
     
+    public ReferenceGenerator(double pos) {
+	ref = pos;
+    }
+    public synchronized void setRef(double pos){
+    	ref = pos;
+    }
     public synchronized double getRef() 
     {
 	return ref;
     }
     
-    public void run() {
-	period = 100;
-	while (true) {
-	    synchronized (this) {
-		ref = amplitude;
-	    }
-	    try {
-		sleep(period);
-	    } catch (Exception x) {}
-	}
-    }
+    
+
 }
 
