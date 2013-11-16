3d06a6328f61795116e5c6f1cf74a75eefcd6d8e
Jerry-Lindstrom <Jerrylindstrom@hotmail.se>
2013-11-16 12:14:25 +0100

    Synchronization and minimized delay verified.

--- Android/src/main/java/com/realtime/project/control/BeamAndBallRegul.java
+++ Android/src/main/java/com/realtime/project/control/BeamAndBallRegul.java
@@ -17,18 +17,16 @@ public class BeamAndBallRegul extends Thread {
 	private double uMax = 10.0;
 	
 	public BeamAndBallRegul(ReferenceGenerator ref, int pri, OutputStream outStream) {
-		// 	analogInPos = beam.getSource(0);
-		// 	analogInAng = beam.getSource(1);
-	// 		analogOut = beam.getSink(0);
-	// 		analogRef = beam.getSink(1);
 		this.refGen = ref;
 		this.outStream = outStream;
 		outer = new PID("PID");
 		inner= new PI("PI");
 		setPriority(pri);
 	}
-
-	private double limit(double u, double umin, double umax) {
+	//synchronization is handled in the run method. 
+	//This method shall not be synchronized, otherwise 
+	//the inner and the outer controller will interfere with eachother.
+	private double limit(double u, double umin, double umax) { 
 		if (u < umin) {
 			u = umin;
 		} else if (u > umax) {
@@ -36,33 +34,33 @@ public class BeamAndBallRegul extends Thread {
 		}
 		return u;
 	}
-	
+	//not used in the run method, Synchronized for external usage.
 	public synchronized void setPosition(double position) {
 		this.positionIn = position;
 	}
-	
+	//not used in the run method, Synchronized for external usage.
 	public synchronized void setAngle(double angle) {
 		this.angleIn = angle;
 	}
-	
+	//not used in the run method, Synchronized for external usage.
 	public synchronized void setInnerParameters(PIParameters p) {
 		inner.setParameters((PIParameters) p.clone());
 	}
-
+	//not used in the run method, Synchronized for external usage.
 	public synchronized PIParameters getInnerParameters() {
 		return inner.getParameters();
 	}
-
+	//not used in the run method, Synchronized for external usage.
 	public synchronized void setOuterParameters(PIDParameters p) {
 		outer.setParameters((PIDParameters)p.clone());
 		Log.d("REGUL", "PID: " + p.K + " " + p.Ti + " " + p.integratorOn);
 	}
-
+	//not used in the run method, Synchronized for external usage.
 	public synchronized PIDParameters getOuterParameters(){
 		return outer.getParameters();
 	}
-	
-	public synchronized void stopRun() {
+	//not used in the run method, Synchronized for external usage.
+	public synchronized void stopRun() { 
 		doRun = false;
 	}
 	
@@ -73,15 +71,13 @@ public class BeamAndBallRegul extends Thread {
 		while (doRun) {			
 			double ref = refGen.getRef();
 			double u;
-			synchronized (outer) {
+			synchronized (outer) { //limit and update state synchronized to outer controller
 				u = limit(outer.calculateOutput(positionIn, ref), uMin, uMax);
 				outer.updateState(u);
 			}
 			double u2 = 100;
-			synchronized (inner) {
+			synchronized (inner) { //limit, update state and outSteam synchronized to inner controller
 				u2 = limit(inner.calculateOutput(angleIn, u), uMin, uMax);
-				//analogOut.set(u2);
-				// SEND TO COMOUTER u2
 				String controlSignal = "CON," + u2;
 				byte[] buffer = controlSignal.getBytes();
 				try {
@@ -91,14 +87,12 @@ public class BeamAndBallRegul extends Thread {
 				}
 				inner.updateState(u2);
 			}
-			//analogRef.set(ref);
 			long duration;
 			t = t + inner.getHMillis();
-			
 			//update plot
 			String temp = "" + u2;
 			updatePlot(ref, positionIn);
-			duration = t - System.currentTimeMillis();
+			duration = t - System.currentTimeMillis(); //duration calculated here to minimize calculation errors due to delay
 			if (duration > 0) {
 				try {
 					sleep(duration);
@@ -107,7 +101,7 @@ public class BeamAndBallRegul extends Thread {
 			}
 		}
 	}
-	public String updatePlot(double ref, double position){
+	public String updatePlot(double ref, double position){ //shall not be synchronized, because it will interfere with the regulators.
 		String out = Double.toString(ref);
 		out += ",";
 		out += Double.toString(position);
