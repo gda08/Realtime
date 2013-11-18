10ac6333fd7061d37c4c2cf76b92d11e26d78f75
hampe1989 <hampe_1989@hotmail.com>
2013-11-18 16:18:10 +0100

    Update WriteCommServer.java

--- PC/WriteCommServer.java
+++ PC/WriteCommServer.java
@@ -1,67 +1,89 @@
 
-
 import java.io.IOException;
 import java.io.OutputStream;
 
 import javax.microedition.io.StreamConnection;
 
+import se.lth.control.realtime.AnalogIn;
+import se.lth.control.realtime.IOChannelException;
+
 import SimEnvironment.AnalogSource;
 
-
 public class WriteCommServer extends Thread {
-	
+
 	private StreamConnection mConnection;
 	private OutputStream outputStream;
-	
-	private AnalogSource analogInPos;
-    private AnalogSource analogInAng;
-    
-    private boolean doRun = true;
+
+	private AnalogIn analogInPos;
+	private AnalogIn analogInAng;
+
+	private boolean doRun = true;
 
 	public WriteCommServer(StreamConnection connection, BeamAndBall beam) {
 		mConnection = connection;
-		analogInPos = beam.getSource(0);
-        analogInAng = beam.getSource(1);
+		try {
+			analogInPos = new AnalogIn(1);
+			analogInAng = new AnalogIn(0);
+		} catch (IOChannelException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 		try {
 			outputStream = mConnection.openOutputStream();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	@Override
 	public void run() {
 		while (true) {
 			synchronized (this) {
-				if (outputStream!=null) {
-					sendPos();
-					sendAng();
+				if (outputStream != null) {
+					sendPosAndAng();
+//					sendAng();
 				}
 			}
 			try {
-				sleep(3000);
+				sleep(100);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
 		}
 	}
-	
-	private void sendPos(){
-		String s = "POS,"+analogInPos.get();
-    	try {
+
+	private synchronized void  sendPosAndAng() {
+		String s;
+		try {
+			String tempPos = "" + analogInPos.get();
+			String tempAng = "" + analogInAng.get();
+//			if(tempPos.contains("E")){
+//				tempPos = "0";
+//			}
+//			if(tempAng.contains("E")){
+//				tempPos = "0";
+//			}
+			s = tempPos;
+			s += "," + tempAng;
+			System.out.println(s);
 			outputStream.write(s.getBytes());
-		} catch (IOException e) {
-			e.printStackTrace();
+		} catch (Exception e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
 		}
 	}
-	
-	private void sendAng(){
-		String s = "ANG,"+analogInAng.get();
-    	try {
+
+	private void sendAng() {
+		String s;
+		try {
+			s = "ANG," + analogInAng.get();
+			System.out.println(s);
 			outputStream.write(s.getBytes());
-		} catch (IOException e) {
-			e.printStackTrace();
+		} catch (Exception e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
 		}
+
 	}
-	
+
 }
